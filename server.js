91a7f56450e9ad33a11042a5a75d3f9240fd8fd4
Ronit Nuguru <ronit.nuguru@gmail.com>
2017-06-12 11:42:26 -0600

    run

--- server.js
+++ server.js
@@ -3,4 +3,12 @@ var app = express();
 
 app.use(express.static(__dirname + '/'));
 
-app.listen(process.env.PORT || 8080);
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
